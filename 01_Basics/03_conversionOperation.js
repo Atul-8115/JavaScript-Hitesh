9633a013432db62d77446b179c153be331d4056a
Atul-8115 <91093034+Atul-8115@users.noreply.github.com>
2023-10-08 14:00:16 +0000

    Last commit

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -23,5 +23,22 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************************** Operations **********************************************
+
+let value = 3;
+let negValue = -value
+// console.log(negValue);
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
