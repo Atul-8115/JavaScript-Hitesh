7d3c943581ac5c7c3ca4f149cdccd3ca626bf325
Atul-8115 <91093034+Atul-8115@users.noreply.github.com>
2023-10-15 14:22:53 +0000

    Today's third commit

--- 03_Basics/02_Arrow.js
+++ 03_Basics/02_Arrow.js
@@ -4,7 +4,34 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username}, welcome to website`);
+        console.log(this);
     }
 }
 
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage();
+
+// console.log(this);
+
+// function chai() {
+//     let username = "atul"
+//     console.log(this.username);
+// }
+
+
+
+// chai();
+
+// const cahi = () => {
+//     let username = "atul"
+//     console.log(this);
+// }
+// cahi();
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+const addTwo = (num1,num2) => (num1 + num2) //Implicit return
+console.log(addTwo(3,4));
