5d740838bbf05806b08cf3e49c148dcf32f8c1b2
Atul-8115 <91093034+Atul-8115@users.noreply.github.com>
2023-10-17 16:19:37 +0000

    Today's first Commit

---
+++ 04_controlFlow/01_switch.js
@@ -0,0 +1,26 @@
+const month = 3;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    default:
+        console.log("default match");
+        break;
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
