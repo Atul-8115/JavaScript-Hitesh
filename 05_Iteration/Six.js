a2d6f151e269ee86b05ec4efef5aefb41917531f
Atul-8115 <91093034+Atul-8115@users.noreply.github.com>
2023-10-22 14:21:11 +0000

    Today's third commit

---
+++ 05_Iteration/Six.js
@@ -0,0 +1,8 @@
+const myNumber = [1,2,3,4,5,6,7,8,9,10] 
+
+// const newNums = myNumber.map( (num) => num + 10)
+const newNums = myNumber
+                .map((num) => num * 10)
+                .map( (num) => num + 1)
+                .filter((num) => num > 21)
+console.log(newNums);
