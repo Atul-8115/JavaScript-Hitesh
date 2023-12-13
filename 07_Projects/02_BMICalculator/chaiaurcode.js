944aeeb0685120ac656c6627d98acc2c5283711e
Atul-8115 <91093034+Atul-8115@users.noreply.github.com>
2023-12-13 14:43:49 +0000

    Second Project completed

--- 07_Projects/02_BMICalculator/chaiaurcode.js
+++ 07_Projects/02_BMICalculator/chaiaurcode.js
@@ -7,10 +7,18 @@ form.addEventListener('submit', function(event) {
     const weight = parseInt(document.querySelector('#weight').value)
     const result = document.querySelector("#results")
 
-    if(height == '' || height < 0 || isNaN(height)) {
+    if(height === '' || height < 0 || isNaN(height)){
         result.innerHTML = `Please enter valid height ${height}`;
     }
-    else if(weight == '' || weight < 0 || isNaN(weight)) {
-        result.innerHTML = `Please enter valid height ${weight}`;
+    else if(weight === '' || weight < 0 || isNaN(weight)) {
+        result.innerHTML = `Please enter valid weight ${weight}`;
+    }
+    else {
+        const bmi = (weight / ((height*height)/10000)).toFixed(3);
+        let str = "";
+        if(bmi < 18.6) str = "Under Weight"
+        else if(bmi >= 18.6 && bmi <= 24.9) str = "Normal Range"
+        else str = "Overweight"
+        result.innerHTML = `<span>${bmi} <br/> ${str}</span>`;
     }
 })
